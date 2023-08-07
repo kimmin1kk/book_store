d73734497378141c49b595282bd155339c477fa0
kimmin1kk <101302590+kimmin1kk@users.noreply.github.com>
2023-08-07 13:37:38 +0900

    Feat: 책별 페이지 기능 추가 + 템플릿 상속 경로 절대경로로 변경

--- src/main/java/com/example/book_store/home/controller/HomeController.java
+++ src/main/java/com/example/book_store/home/controller/HomeController.java
@@ -8,12 +8,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
 import com.example.book_store.product.domain.Product;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -62,9 +64,15 @@ public class HomeController {
         return "account/myPage";
     }
 
-    @GetMapping("/singleProduct")
-    public String singleProduct(Model model) {
+    @GetMapping("/single-product/{seq}")
+    public String singleProduct(Model model, @PathVariable("seq") Long seq) {
         log.info("HomeController -> singleProduct : OK");
+        Optional<Product> productOptional = homeService.findProductBySeq(seq);
+
+        if (productOptional.isPresent()) {
+            Product product = productOptional.get();
+            model.addAttribute("product", product);
+        }
         return "shop/singleProduct";
     }
 
